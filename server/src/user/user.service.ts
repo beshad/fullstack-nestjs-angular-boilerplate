2b08204428d960113a5330be8cce8d09b1d83faf
Behshad Ghorbani <behshad.ghorbani@truescape.com>
2019-08-20 15:48:21 +1200

    server updated

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -6,31 +6,31 @@ import { CreateUserDTO } from './dto/create-User.dto';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectModel('User') private readonly UserModel: Model<User>) { }
-    // fetch all Users
-    async getAllUser(): Promise<User[]> {
-        const Users = await this.UserModel.find().exec();
-        return Users;
-    }
-    // Get a single User
-    async getUser(UserID): Promise<User> {
-        const User = await this.UserModel.findById(UserID).exec();
-        return User;
-    }
-    // post a single User
-    async addUser(createUserDTO: CreateUserDTO): Promise<User> {
-        const newUser = await this.UserModel(createUserDTO);
-        return newUser.save();
-    }
-    // Edit User details
-    async updateUser(UserID, createUserDTO: CreateUserDTO): Promise<User> {
-        const updatedUser = await this.UserModel
-            .findByIdAndUpdate(UserID, createUserDTO, { new: true });
-        return updatedUser;
-    }
-    // Delete a User
-    async deleteUser(UserID): Promise<any> {
-        const deletedUser = await this.UserModel.findByIdAndRemove(UserID);
-        return deletedUser;
-    }
+  constructor(@InjectModel('User') private readonly UserModel: Model<User>) { }
+  // fetch all Users
+  async getAllUser(): Promise<User[]> {
+    const Users = await this.UserModel.find().exec();
+    return Users;
+  }
+  // Get a single User
+  async getUser(UserID): Promise<User> {
+    const User = await this.UserModel.findById(UserID).exec();
+    return User;
+  }
+  // post a single User
+  async addUser(createUserDTO: CreateUserDTO): Promise<User> {
+    const newUser = await this.UserModel(createUserDTO);
+    return newUser.save();
+  }
+  // Edit User details
+  async updateUser(UserID, createUserDTO: CreateUserDTO): Promise<User> {
+    const updatedUser = await this.UserModel
+      .findByIdAndUpdate(UserID, createUserDTO, { new: true });
+    return updatedUser;
+  }
+  // Delete a User
+  async deleteUser(UserID): Promise<any> {
+    const deletedUser = await this.UserModel.findByIdAndRemove(UserID);
+    return deletedUser;
+  }
 }
