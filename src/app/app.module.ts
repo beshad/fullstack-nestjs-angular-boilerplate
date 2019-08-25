45f11de36990696dfeca074930321f6e93f46553
Behshad Ghorbani <behshad.ghorbani@truescape.com>
2019-08-26 10:55:43 +1200

    bootstrap, ngx-bootrsap and angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+import { NgMaterialModule } from './app-material/app-material.module';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    AppBootstrapModule,
+    BrowserAnimationsModule,
+    NgMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
